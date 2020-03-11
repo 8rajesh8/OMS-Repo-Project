531bad169da7581fc8acf33859d8185b6e9c5e5d
Fire Bender <Fire Bender@172.21.235.222>
2020-03-11 14:53:35 +0530

    commit

---
+++ spring_rest_invoice/src/main/java/com/example/demo/SpringRest11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest11Application.class, args);
+	}
+
+}
